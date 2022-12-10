516444b1741cf55030d372a4ee0e7a9e616195bf
Thomas Ohms <thomas@ohms.pro>
2022-12-10 14:22:29 +0100

    feat(uebung2): Loesung

---
+++ aufgaben/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container px-6 py-16 prose">
+    <div id="tasks-header">
+      <h1 class="text-center">Aufgabenliste</h1>
+      <button class="btn btn-primary">Aufgabe hinzufügen</button>
+      <hr>
+    </div>
+    <div id="tasks-list">
+      <section>
+        <section id="no-tasks">Keine Aufgaben verfügbar</section>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
